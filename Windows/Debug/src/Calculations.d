d147f41ebaec8d7ddd29a38b0456388eb76558a6
arthurmz <arthur.jmacedo@gmail.com>
2016-03-21 10:34:13 -0300

    Multiplataforma

---
+++ Windows/Debug/src/Calculations.d
@@ -0,0 +1,11 @@
+src/Calculations.o: \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Calculations.c \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Calculations.h \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/NSGAII.h \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/StaticVariables.h
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Calculations.h:
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/NSGAII.h:
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/StaticVariables.h:
---
+++ Windows/Debug/src/RidesharingNSGAII.d
@@ -0,0 +1,14 @@
+src/RidesharingNSGAII.o: \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/RidesharingNSGAII.c \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Helper.h \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/NSGAII.h \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/StaticVariables.h \
+ C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Calculations.h
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Helper.h:
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/NSGAII.h:
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/StaticVariables.h:
+
+C:/Users/SIGAA-Des/UFRN/workspace/RidesharingNSGA-II/src/Calculations.h:
